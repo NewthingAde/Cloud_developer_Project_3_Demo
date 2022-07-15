f225b07d880823fd5e1b97a8086cfd38e8388dbb
Oluwasegun <80678596+NewthingAde@users.noreply.github.com>
2022-07-15 22:32:22 +0200

    project microservice

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=myusername
+export POSTGRES_PASSWORD=mypasssword
+export POSTGRES_HOST=database-1.comb4hoh2nqs.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=mono123456
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
