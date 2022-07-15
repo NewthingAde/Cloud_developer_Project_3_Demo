64c7fb06d9c7bef354c3029613789b003eba8bd0
Oluwasegun <80678596+NewthingAde@users.noreply.github.com>
2022-07-16 00:44:34 +0200

    complete files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aebe6bcb176a047619190e503c6b8396-1508785488.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab066fd1a1d974a74afaa9284168c64c-1741910681.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
